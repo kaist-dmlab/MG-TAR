51de47d07693414e431403b0fd6f0363d4fe545f
Patara Trirat <patara.t@kaist.ac.kr>
2023-11-11 04:00:51 +0900

    Update README.md

--- README.md
+++ README.md
@@ -60,3 +60,4 @@ Due to the continuing colossal socio-economic losses caused by traffic accidents
  
  
  
+ 
--- r.sh
+++ r.sh
@@ -1,9 +1,15 @@
-sed -i -e '$a\ ' README.md
-git remote add origin https://github.com/kaist-dmlab/MG-TAR.git
-git add *
-git commit -m "Update README.md"
-git push origin
-git config credential.helper store
-git config credential.helper cache
-git config credential.helper 'cache --timeout=180000'
+max=212
+for i in `seq 2 $max`
+do
+    sed -i -e '$a\ ' README.md
+    git remote add origin https://github.com/kaist-dmlab/MG-TAR.git
+    git add *
+    git commit -m "Update README.md"
+    git push origin
+    git config credential.helper store
+    git config credential.helper cache
+    git config credential.helper 'cache --timeout=180000'
+done
+
+
 
